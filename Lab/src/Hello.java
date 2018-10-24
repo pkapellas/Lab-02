c361639c307b4aa29e90d08709d4b9b6bc60c5a0
Jingwei <Jingwei@10.118.73.2>
2018-10-24 13:26:29 -0700

    2nd line

--- Lab/src/Hello.java
+++ Lab/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Git!");
+		System.out.println("2nd Commit");
 	}
 
 }
