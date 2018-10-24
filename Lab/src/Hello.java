c35978c12380f35f54e1c859cd41accd66c7886b
Jingwei <Jingwei@10.118.73.2>
2018-10-24 13:58:14 -0700

    update again

--- Lab/src/Hello.java
+++ Lab/src/Hello.java
@@ -3,9 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
-		System.out.println("yang - This line is added by Dr. Yang.");
+		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line is added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 }
 
 }
